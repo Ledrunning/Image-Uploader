6ed16a5f44c73548ae6bb936fd599a8a327d53a5
Ledrunning <mazzinov@gmail.com>
2018-06-07 21:07:32 +0300

    UploadImage method has been added

--- Uploader/MainWindow.xaml.cs
+++ Uploader/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,8 @@ namespace Uploader
     public partial class MainWindow : Window
     {
         byte[] image;
+        private string FileName { get; set; }
+            
         private readonly string url = "http://127.0.0.1:18710/";
 
         public MainWindow()
@@ -35,13 +38,15 @@ namespace Uploader
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            
-            
-            openFileDialog.Filter = "JPEG(*.jpg)|*.jpg|All(*.*)|*";
            
+
+
+            openFileDialog.Filter = "JPEG(*.jpg)|*.jpg|All(*.*)|*";
+            
             if (openFileDialog.ShowDialog() == true)
             {
-               try
+                FileName = openFileDialog.SafeFileName;
+                try
                 {
                     image = File.ReadAllBytes(openFileDialog.FileName);
                     MessageBox.Show("File has been opened");
@@ -55,15 +60,28 @@ namespace Uploader
               
         }
 
-        private async Task Upload_ClickAsync(object sender, RoutedEventArgs e)
+        private void Upload_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+                       
+            
+            
+            MessageBox.Show("File has been uploaded");
+        }
+
+        async public Task<HttpResponseMessage> UploadImage(string url, byte[] ImageData)
         {
             var client = new HttpClient();
 
-                       
-            ByteArrayContent byteContent = new ByteArrayContent(image);
-            HttpResponseMessage reponse = await client.PostAsync(url, byteContent);
+            var requestContent = new MultipartFormDataContent();
             
-            MessageBox.Show("File has been uploaded");
+            var imageContent = new ByteArrayContent(ImageData);
+            imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+
+            requestContent.Add(imageContent, FileName);
+
+            return await client.PostAsync(url, requestContent);
         }
     }
 }
